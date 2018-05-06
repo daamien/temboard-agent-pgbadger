2b837cc5e73bc2ceac606a985d15f28a49cbf606
damien clochard <damien@dalibo.com>
2018-05-06 16:11:45 +0200

    check_version : init

--- pgbadger/__init__.py
+++ pgbadger/__init__.py
@@ -13,18 +13,19 @@ from . import pgbadger
 # API Entrypoints
 #
 
+routes = RouteSet()
+
 @routes.get(b'/pgbadger/version')
 def get_pgbadger_version(http_context, app):
-    return pgbadger.check_pgbadger_version(conn)
+    return pgbadger.check_pgbadger_version()
 
 logger = logging.getLogger(__name__) 
 
-routes = RouteSet()
 
 #
-# 
+# load/unload plugin  
 #
-class PgbadgerPlugin(object):
+class pgbadgerPlugin(object):
 
     def __init__(self, app, **kw):
         self.app = app
@@ -32,14 +33,16 @@ class PgbadgerPlugin(object):
     def load(self):
 	logger.info('Starting the pgBagder plugin')
 	
-	# if pgBadger is not present, the plugin is useless
 	try:
-	    pgbadger.check_pgbadger_version()
+	    version=pgbadger.check_pgbadger_version()
+            logger.info('Found pgBadger version : %s' %version )
 	except:
-	    logger.warning('Cannot find pgBadger on the server. The pgBadger will not work properly.')
+            # if pgBadger is not present, the plugin is useless
+	    logger.warning('Cannot find pgBadger on the server. The pgBadger plugin will not work properly.')
 	    logger.info('Check that pgBadger is installed on the server.')
 	
 	# create API routes
+        logger.info('Adding pgBadger routes')
         self.app.router.add(routes)
 
     def unload(self):
--- pgbadger/pgbadger.py
+++ pgbadger/pgbadger.py
@@ -2,9 +2,18 @@
 import os
 
 from temboardagent.errors import UserError
+from temboardagent.command import exec_command
+
 
 def check_pgbadger_version():
-    pgbadger_version='x'
-    if not pgbadger_version:
-        msg = "Seems like pgBadger is not installed."
+    """
+    returns the pgBadger version, throws an error is pgBadger is not present 
+    """
+    command=['pgbadger', '--version']
+    (return_code, stdout, stderr) = exec_command(command)
+
+    if return_code!=0:
+        msg = "Seems like pgBadger is not installed : %s" %stderr
         raise UserError(msg)
+    
+    return "{ 'version' : '%s' }" %stdout
